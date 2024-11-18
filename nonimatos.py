201d2f300f2ea6dde036839ecaf04536c6b2070d
sessionsvps <dejercito4545@gmail.com>
2024-11-17 21:43:58 -0500

    Add files via upload

---
+++ nonimatos.py
@@ -0,0 +1,98 @@
+from dotenv import load_dotenv
+from telethon.sync import TelegramClient, events
+import os
+import json
+import asyncio
+
+# async def getListOfGroups(client):
+#     try:
+#         dialogs = await client.get_dialogs()
+#         groups_info = []
+#         for dialog in dialogs:
+#             if dialog.is_group or dialog.is_channel:  # Filtra los grupos y canales
+#                 group_info = {'group_id': dialog.id, 'group_name': dialog.title}
+#                 groups_info.append(group_info)
+#                 # Imprimir el nombre y el ID del grupo
+#                 print(f"Grupo: {dialog.title}, ID: {dialog.id}")
+#         return groups_info
+#     except Exception as e:
+#         print(f"Error: {e}")
+#         return []
+
+
+async def getListOfGroups(client):
+    try:
+        dialogs = await client.get_dialogs()
+        groups_info = []
+        for dialog in dialogs:
+            if dialog.is_group or dialog.is_channel:
+                entity = await client.get_entity(dialog.id)
+                can_send_messages = entity.default_banned_rights is None or not entity.default_banned_rights.send_messages
+                if can_send_messages:
+                    group_info = {'group_id': dialog.id, 'group_name': dialog.title}
+                    groups_info.append(group_info)
+
+        return groups_info
+    except Exception as e:
+        print(e)
+        return []
+async def getMessagesFromGroup(client, group_id):
+    try:
+        all_messages = []
+        async for message in client.iter_messages(group_id):
+            try:
+                all_messages.append(message)
+            except:
+                pass
+        return all_messages
+    except Exception as e:
+        print(e)
+        return []
+async def logUserBot():
+    load_dotenv()
+    api_id = int(20726101)
+    api_hash = "a7ab1500140e5c2ff7c15fdc27de8936"
+    phone_number = "51965949449"
+    session_name = "bot_spammer"
+    client = TelegramClient(session_name, api_id, api_hash)
+    await client.connect()
+    if not await client.is_user_authorized():
+        await client.send_code_request(phone_number)
+        await client.sign_in(phone_number, input('Ingrese el código de verificación: '))
+    await client.send_message("@spambot2024qc", f'<b>Bot encendido</b>', parse_mode="HTML")
+    spammer_group = int("-4500212428")
+    excluded_groups = [-1002171101200,-1001926519077,-1001907073788,-1001918809414,-1002224408369,-1002095862989,-1001586028282,-1001829996546,-1002112968455,-1002001880141,-4527438300,-4532673979,-1002304840729,-1002291580489]
+
+    while True:
+        groups_info = await getListOfGroups(client)
+        messages_list = await getMessagesFromGroup(client, spammer_group)
+            
+        try:
+            await client.send_message("@spambot2024qc", f"<b>CANTIDAD DE MENSAJES CONSEGUIDOS PARA PUBLICAR</b> <code>{len(messages_list)-1}</code>",parse_mode="HTML")
+        except:
+            pass
+            
+        try:
+            for i in groups_info:
+                if i['group_name'] not in ["Spam 2024","Spam","RESPALDO🇵🇪BINS PERU","➳𝒀𝑨𝑷𝑬 𝑫𝑬 𝑬𝑺𝑻𝑨𝑭𝑨𝑫𝑶𝑹𝑬𝑺 ✧","QUEMANDO ESTAFADORES","𝐏𝐄𝐑Ú 𝐀𝐘𝐔𝐃𝐀","Referencias Elmer 💸","🎭 CANAL MUNDO STREAMING PERÚ 🇵🇪","TU MARKETPLACE"] and i['group_id'] not in excluded_groups:
+                    j=0
+                    for message_spam in messages_list:
+                        j+=1
+                        resultado = True
+                        try:
+                            await client.forward_messages(i["group_id"], message_spam)
+                        except Exception as error:
+                            await client.send_message("@spambot2024qc", f'<b>Error enviando mensajes a {i["group_id"]}</b> - <code>{i["group_name"]}<code>\nCausa:{error}',parse_mode="HTML")
+                            resultado = False
+                        if resultado:
+                            await client.send_message("@spambot2024qc", f'<b>Mensaje enviado a {i["group_id"]}</b> - <code>{i["group_name"]}</code>',parse_mode="HTML")  
+                        await asyncio.sleep(90)
+                        if j==1: break
+            await client.send_message("@spambot2024qc", f'<b>RONDA ACABADA</b>', parse_mode="HTML")
+            await asyncio.sleep(300) 
+        except:
+            pass
+    
+            
+if __name__ == "__main__":
+    asyncio.run(logUserBot())
